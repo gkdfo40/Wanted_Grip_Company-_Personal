d4d6a329a2e4bb1bc86f4e910c382f1f840ec79b
gkdfo40 <gkdfo40@gmail.com>
2022-05-14 17:26:27 +0900

    fix: delete warnning

--- src/components/BookMark/BookMark.tsx
+++ src/components/BookMark/BookMark.tsx
@@ -1,17 +1,81 @@
-import { useRecoilValue } from 'recoil'
+import { DragEvent, useState } from 'react'
+import { useRecoilState } from 'recoil'
 
 import styles from './BookMark.module.scss'
 
 import MovieItem from 'components/ItemCard/ItemCard'
 import { bookMarkList } from 'states/state'
+import { IMovie } from 'types/movie'
 
 interface BookMarkProps{
   openModal:()=>void
 }
 
+interface IDND{
+  draggedFrom: number
+  draggedTo: number
+  isDragging: boolean
+  originalOrder: IMovie[]
+  updataedOrder: IMovie[]
+}
+
 const BookMark = ({openModal }: BookMarkProps) => {
 
-  const bookMarks = useRecoilValue(bookMarkList)
+  const [bookMarks, setBookMarks] = useRecoilState(bookMarkList)
+  const [list, setList] = useState<IMovie[]>(bookMarks)
+  const [dragAndDrop, setDragAndDrop] = useState<IDND>({
+    draggedFrom: -1,
+    draggedTo: -1,
+    isDragging: false,
+    originalOrder: [],
+    updataedOrder:[]
+  })
+
+  const onDragStart = (e: DragEvent<HTMLLIElement>) => {
+    const initialPosition = parseInt(e.currentTarget.dataset.position as string, 10)
+    setDragAndDrop({
+      ...dragAndDrop,
+      draggedFrom: initialPosition,
+      originalOrder:list
+    })
+  }
+  const onDragOver = (e: DragEvent<HTMLLIElement>) => {
+    e.preventDefault()
+    let newList = dragAndDrop.originalOrder
+    const { draggedFrom } = dragAndDrop
+    const draggedTo = parseInt(e.currentTarget.dataset.position as string, 10)
+    const itemDragged = newList[draggedFrom]
+    const remainingItems = newList.filter((_, index) => index !== draggedFrom)
+    newList = [...remainingItems.slice(0, draggedTo), itemDragged, ...remainingItems.slice(draggedTo),]
+    if (draggedTo !== dragAndDrop.draggedTo) {
+      setDragAndDrop({
+        ...dragAndDrop,
+        updataedOrder: newList,
+        draggedTo
+      })
+    }
+  }
+
+  const onDrop = () => {
+    setList(dragAndDrop.updataedOrder)
+    setDragAndDrop({
+      ...dragAndDrop,
+      draggedFrom: -1,
+      draggedTo:-1
+    })
+  }
+
+  const onDragEnter = () => {
+  }
+  const onDragLeave = () => {
+    setDragAndDrop({
+      ...dragAndDrop,
+      draggedTo:-1
+    })
+  }
+  const onDragEnd = () => {
+    setBookMarks(list)
+  }
   
   return (
     <div className={styles.bookMarkContainer}>
@@ -22,7 +86,27 @@ const BookMark = ({openModal }: BookMarkProps) => {
         <ul className={styles.movieItemList}>
           <li className={styles.emptyHead} />
           {
-            bookMarks.map((movie) => <MovieItem key={movie.imdbID} movie={movie} openModal={openModal} />)
+            bookMarks.map((movie,index) =>
+              <li
+                key={movie.imdbID}
+                data-position={index}
+                role='menuitem'
+                draggable='true'
+                onDragStart={onDragStart}
+                onDragOver={onDragOver}
+                onDrop={onDrop}
+                onDragLeave={onDragLeave}
+                onDragEnter={onDragEnter}
+                onDragEnd={onDragEnd}
+              >
+                <MovieItem
+                  key={movie.imdbID}
+                  movie={movie}
+                  openModal={openModal}
+                />
+              </li>
+              
+            )
           }
           <li className={styles.emptyFooter}>{bookMarks.length === 0 && '북마크 목록이 없습니다.'}</li>
         </ul>
--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -13,27 +13,25 @@ interface ItemProps{
 const Item = ({ movie, openModal }: ItemProps) => {
 
   const target = useSetRecoilState<IMovie>(pickMovie)
-
+  
   const handleClickItem = () => {
     target(movie)
     openModal()
   }
 
   return (
-    <li key={movie.imdbID} role='menuitem' onClick={handleClickItem}>
-      <article className={styles.item}>
-        <div className={styles.poster}>
-          <img src={movie.Poster === 'N/A' ? NoImage : movie.Poster} alt='Poster' />
+    <article className={styles.item} onDoubleClick={handleClickItem}>
+      <div className={styles.poster}>
+        <img src={movie.Poster === 'N/A' ? NoImage : movie.Poster} alt='Poster' draggable='false' />
+      </div>
+      <div className={styles.content}>
+        <p className={styles.title}>{movie.Title}</p>
+        <div>
+          <p className={styles.year}>{movie.Year.slice(0,4)}</p>
+          <p className={styles.type}>{movie.Type.toUpperCase()}</p>
         </div>
-        <div className={styles.content}>
-          <p className={styles.title}>{movie.Title}</p>
-          <div>
-            <p className={styles.year}>{movie.Year.slice(0,4)}</p>
-            <p className={styles.type}>{movie.Type.toUpperCase()}</p>
-          </div>
-        </div>
-      </article>  
-    </li>
+      </div>
+    </article>  
   )
 }
 
